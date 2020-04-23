867a0574a8cd8691350cef03dd8645a12d290464
Eestela <augustinestela@gmail.com>
2020-04-23 14:59:24 +0200

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     int t;
 
 	r = ft_printf("|le mien {%10.8d}|\n", 42);
-	t = printf("|le vrai {%10.8d}|\n", 42);
+	t =    printf("|le vrai {%10.8d}|\n", 42);
     printf("r = %i\nt = %i\n", r, t);
 
 }
