be778d88e90395b89fe13a62cbf75b2e3a45005f
Eestela <augustinestela@gmail.com>
2020-05-02 18:24:57 +0200

    fghjklpoeeddwwwsf

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include "ft_printf.h"
 
+int inter(char *form, ...)
+{
+    va_list ap;
+    t_oneforall *lst;
+
+    lst = malloc(sizeof(t_oneforall));
+    initlst(lst, form);
+    lst->width = 10;
+    va_start(ap, form);
+    return(printint(lst, ap));
+}
 int main()
 {
     int r;
     int t;
 
-	r = ft_printf("|le mien {%10.8d}|\n", 42);
-	t =    printf("|le vrai {%10.8d}|\n", 42);
+
+	
+	t = inter("%10.8d|", 42);
     printf("r = %i\nt = %i\n", r, t);
 
 }
